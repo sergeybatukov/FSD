19e0c96f6f4ee3bd3f279aeac4eb7805ca52a6d9
sashachepuha <shizadance69@gmail.com>
2020-01-22 00:20:42 +0300

    Обработка шрифтов + Pug
    package.json:
    +pug
    +pug-loader
    
    webpack.config.js:
    +пути вынесены в переменную (необходимо заменить в остальных частях кода)
    +обработка .pug файлов
    +обработка шрифтов
    +автоматическое добавление страниц (из .pug в .html)

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 const path = require('path')
+const fs = require('fs')
 const MiniCssExtractPlagin = require('mini-css-extract-plugin')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const PATHS = {
+    src: path.join(__dirname, '../FSD/src'),
+    dist: path.join(__dirname, '../FSD/dist'),
+    assets: 'assets/'
+}
+
+const PAGES_DIR = `${PATHS.src}/pug/pages/`
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
 
 module.exports = {
     entry: {
@@ -14,10 +24,25 @@ module.exports = {
     },
 
     module: {
-        rules: [{
+        rules: [
+        {
+            test: /\.pug$/,
+            oneOf: [
+                {
+                    use: ['pug-loader']
+                }]
+        },    
+        {
             test: /\.js/,
             loader: 'babel-loader',
             exclude: '/node_modules/'
+        },
+        {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'file-loader',
+            options:{
+                name: "[name].[ext]"
+            }
         },
         {
             test: /\.(png|jpg|gif|svg)/,
@@ -75,13 +100,13 @@ module.exports = {
         new MiniCssExtractPlagin({
             filename: "assets/[name].css"
         }),
-        new HtmlWebPackPlugin({
-            hash: false,
-            template: 'src/index.html',
-            filename: './index.html'
-        }),
         new CopyWebpackPlugin([
-            {from: 'src/img/', to: 'assets/img'}
-        ]) 
+            {from: 'src/img/', to: 'assets/img'},
+            {from: 'src/fonts/', to: 'assets/fonts'}
+        ]),
+        ...PAGES.map(page => new HtmlWebpackPlugin({
+            template: `${PAGES_DIR}/${page}`,
+            filename: `./${page.replace(/\.pug/,'.html')}`
+        }))
     ]
 }
