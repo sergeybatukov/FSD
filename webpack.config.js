20bb8623035ccb4160018cf5ba053a7600cd4965
sashachepuha <shizadance69@gmail.com>
2020-01-21 16:54:35 +0300

    Update webpack.config.js
    +правила обработки css и scss
    +обработка с использованием mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const MiniCssExtractPlagin = require('mini-css-extract-plugin')
 
 
 module.exports = {
@@ -16,10 +17,54 @@ module.exports = {
             test: /\.js/,
             loader: 'babel-loader',
             exclude: '/node_modules/'
-        }]
+        },
+        {
+            test: /\.css/,
+            use: [
+                "style-loader",
+                MiniCssExtractPlagin.loader, 
+                {
+                    loader: "css-loader",
+                    options: {sourceMap: true}
+                },
+                {
+                    loader: "postcss-loader",
+                    options: {sourceMap: true, config: { path: 'src/js/postcss.config.js' }}
+                },
+                {
+                    loader: "sass-loader",
+                    options: {sourceMap: true}
+                }
+            ]
+        },
+        {
+            test: /\.scss/,
+            use: [
+                "style-loader",
+                MiniCssExtractPlagin.loader, 
+                {
+                    loader: "css-loader",
+                    options: {sourceMap: true}
+                },
+                {
+                    loader: "postcss-loader",
+                    options: {sourceMap: true, config: { path: 'src/js/postcss.config.js' }}
+                },
+                {
+                    loader: "sass-loader",
+                    options: {sourceMap: true}
+                }
+            ]
+        }
+    ]
     },
 
     devServer: {
         overlay: true
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlagin({
+            filename: "[name].css"
+        })
+    ]
 }
