c61a0cc7616672c6aed06d8f5457a319464226ea
sashachepuha <shizadance69@gmail.com>
2020-02-02 13:47:07 +0300

    Исправлена ошибка в скрипте для dropdown
    +измеяется тот элемент с которым взаимдействует пользаватель

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -4,20 +4,23 @@ $(document).ready(function() {
     var one = 0
     var two = 0
     var tree = 0
+
     $( ".dropdown__value" ).click(function(){
-        $( ".dropdown__list" ).slideToggle(400);
-        if ($('.dropdown').hasClass('dropdown__border')){
-            $('.dropdown').removeClass('dropdown__border');
+        $(this).siblings(".dropdown__list" ).slideToggle(400);
+        if ($(this).parent('.dropdown').hasClass('dropdown__border')){
+            $(this).parent('.dropdown').removeClass('dropdown__border');
         }
         else{
-            $('.dropdown').addClass('dropdown__border');
+            $(this).parent('.dropdown').addClass('dropdown__border');
         }
     });
+    
     $(".dropdown__plus").click(function(){
         val = +$(this).siblings(".dropdown__item-val").html()
         val += 1
         $(this).siblings(".dropdown__item-val").html(val)
     });
+
     $(".dropdown__minus").click(function(){
         val = +$(this).siblings(".dropdown__item-val").html()
         if (val == 0){
@@ -28,6 +31,7 @@ $(document).ready(function() {
         $(this).siblings(".dropdown__item-val").html(val)
         }
     });
+
     $(".dropdown__list .button__text:eq(1)").click(function(){
         one = +$( ".dropdown__list .dropdown__item-val:eq(0)" ).html()
         two = +$( ".dropdown__list .dropdown__item-val:eq(1)" ).html()
@@ -36,21 +40,21 @@ $(document).ready(function() {
             $( ".dropdown__value" ).html(text)
         }
         else if(one+two >= 2 && one+two <=4){
-            $( ".dropdown__value" ).html(one+two+" гостя")
+            $(".dropdown-guests").children( ".dropdown__value" ).html(one+two+" гостя")
         }
         else if(one+two >= 5){
-            $( ".dropdown__value" ).html(one+two+" гостей")
+            $(".dropdown-guests").children( ".dropdown__value" ).html(one+two+" гостей")
         }
         else if(one+two == 1){
-            $( ".dropdown__value" ).html(one+two+" гость")
+            $(".dropdown-guests").children( ".dropdown__value" ).html(one+two+" гость")
         }
             
     });
     $(".dropdown__list .button__text:eq(0)").click(function(){
-        $( ".dropdown__value" ).html(text)
-        $( ".dropdown__list .dropdown__item-val:eq(0)" ).html(0)
-        $( ".dropdown__list .dropdown__item-val:eq(1)" ).html(0)
-        $( ".dropdown__list .dropdown__item-val:eq(2)" ).html(0)
+        $(".dropdown-guests").children( ".dropdown__value" ).html(text)
+        $(".dropdown-guests").children( ".dropdown__list .dropdown__item-val:eq(0)" ).html(0)
+        $(".dropdown-guests").children( ".dropdown__list .dropdown__item-val:eq(1)" ).html(0)
+        $(".dropdown-guests").children( ".dropdown__list .dropdown__item-val:eq(2)" ).html(0)
     });
 });
 
