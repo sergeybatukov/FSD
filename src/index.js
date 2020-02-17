ad6224f7f02698b92429c5399665c0513258907d
sashachepuha <shizadance69@gmail.com>
2020-02-17 11:06:10 +0300

    Добавлен плагин air datepicker
    +подключение в index.js
    +скрипт плагина

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import './scss/main.scss'
 
 import jquery from 'jquery'
 import $ from 'jquery'
+
 import './js/checkbox-list'
 import './js/like-button'
 import './js/dropdown'
 require('webpack-jquery-ui/slider');
+require('./js/plagins/datepicker');
+
 import './js/range-slider'
+import './js/date-range'
