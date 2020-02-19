ea92ffa0fde5f2a9d92e7803be3860260d471e9b
sashachepuha <shizadance69@gmail.com>
2020-02-19 15:51:55 +0300

    Доработал date-range
    +исправил позиционирование
    +убрал из кода скрипта лишнии строки(доработал селектор)

--- src/js/date-range.js
+++ src/js/date-range.js
@@ -19,14 +19,14 @@ $(document).ready(function() {
 
     $(".datepicker" ).slideToggle(0);
 
-    $(".date-range__start").click(function(){
+    $(".date-range__start, .date-range__end, .start").click(function(){
         var position = $(".date-range__start").offset()
         $(".datepicker" ).slideDown(400);
     })
-    $(".date-range__end").click(function(){
-        var position = $(".date-range__start").offset()
-        $(".datepicker" ).slideDown(400);
-    })
+    // $(".date-range__end").click(function(){
+    //     var position = $(".date-range__start").offset()
+    //     $(".datepicker" ).slideDown(400);
+    // })
 
     $('.button__text').not('.button__text__inactive').click(function(){
         $(this).closest('.datepicker').slideToggle(400);
